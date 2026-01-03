86a214e2eb1eb769e662ce40e977de40d8a48a71
Hamilton R <hamiltonjreed@gmail.com>
2026-01-03 08:25:04 -0700

    stub out initial slate of work for managing spaces

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -47,7 +47,16 @@
 	</div>
 {:else}
 	<AppLayout>
+		<!-- TODO all this junk inside the svelte:fragment needs to be extracted-->
 		<svelte:fragment slot="nav">
+			<!-- TODO this button needs to trigger a modal form for submission-->
+			 <!-- TODO add server side api routes for interacting with the SpacesService-->
+			<div class="nav-head">
+				<button type="button">➕</button>
+			</div>
+			{#if data.spaces.length == 0}
+				<div> No Spaces Found</div>
+			{/if}
 			{#each data.spaces as {name, icon}}
 				<div>{icon} -- {name}</div>
 			{/each}
