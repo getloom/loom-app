595207fee92f28386875768de2b06b5f42457f1c
greatbacon <hamiltonjreed@gmail.com>
2026-02-01 21:01:47 -0700

    add notes

--- src/routes/api/spaces/+server.ts
+++ src/routes/api/spaces/+server.ts
@@ -1,5 +1,10 @@
 import { json } from '@sveltejs/kit';
 
+//TODO
+// seems like the most "Svelte" way to do this is to make this a page that returns results
+// then make some post return work in the SpaceNavActions that confirms data and closes the display
+// ideally it should also update the data underneath?
+// it might also just be better to trigger a full page refresh on submission or something
 export function POST(data) {
     console.log(data);
     console.log("inside the api spaces routes")
