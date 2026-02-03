233b4e64b464899c656408560758aa5f1f1e5443
greatbacon <hamiltonjreed@gmail.com>
2026-02-03 12:23:43 -0700

    more notes

--- src/routes/api/spaces/+server.ts
+++ src/routes/api/spaces/+server.ts
@@ -5,6 +5,7 @@ import { json } from '@sveltejs/kit';
 // then make some post return work in the SpaceNavActions that confirms data and closes the display
 // ideally it should also update the data underneath?
 // it might also just be better to trigger a full page refresh on submission or something
+// probably better to start with the API though for backend behavior, add some tests, then connect the front end stub to backend logic
 export function POST(data) {
     console.log(data);
     console.log("inside the api spaces routes")
