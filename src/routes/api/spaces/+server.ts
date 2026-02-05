634b97bfaddb098f08ab17a9632fd5e7394e71f7
Hamilton R <hamiltonjreed@gmail.com>
2026-02-04 19:25:29 -0700

    add first pass create

--- src/routes/api/spaces/+server.ts
+++ src/routes/api/spaces/+server.ts
@@ -1,4 +1,7 @@
-import { json } from '@sveltejs/kit';
+import { defaultPostgresOptions } from '$lib/db/postgres.server';
+import { SpaceRepo } from '$lib/system/spaces/spacesRepo.js';
+import type { Space } from '$lib/system/spaces/spacesService.js';
+import postgres from 'postgres';
 
 //TODO
 // seems like the most "Svelte" way to do this is to make this a page that returns results
@@ -6,10 +9,12 @@ import { json } from '@sveltejs/kit';
 // ideally it should also update the data underneath?
 // it might also just be better to trigger a full page refresh on submission or something
 // probably better to start with the API though for backend behavior, add some tests, then connect the front end stub to backend logic
-export function POST(data) {
-    console.log(data);
+export async function POST({request}): Promise<Space> {
+    console.log(request);
     console.log("inside the api spaces routes")
-	const number = Math.floor(Math.random() * 6) + 1;
-	
-	return json(number);
+	//needed input is name, icon & view type
+    const {name, icon, view} = await request.json();
+    //spin up the db
+    const space = await new SpaceRepo(postgres(defaultPostgresOptions)).create(name, icon, view);
+    return space;
 }
