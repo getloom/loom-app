97a823acf104b551b6064b03ffba4e71320f85af
greatbacon <hamiltonjreed@gmail.com>
2026-04-14 11:33:46 -0600

    implement the rest of accounts crud

--- src/lib/system/accounts/accountsRepo.ts
+++ src/lib/system/accounts/accountsRepo.ts
@@ -1,67 +1,65 @@
-import { Repo } from "$lib/db/repo";
-import type { Account } from "$lib/system/accounts/accountsService";
-import {createSalt, hashPassword} from "$lib/util/crypto.server"
+import { Repo } from '$lib/db/repo';
+import type { Account } from '$lib/system/accounts/accountsService';
+import { createSalt, hashPassword } from '$lib/util/crypto.server';
 
 //TODO replace with a proper logger system
 const log = console;
 
 export class AccountRepo extends Repo {
-    /**
-     * Creates a new account with a salted & hashed password
-     */
-    async create(username: string, clear_password: string): Promise<Account> {
-        const salt = createSalt();
-        const password = hashPassword(clear_password,salt)
-        const data = await this.sql<Account[]>`
+	/**
+	 * Creates a new account with a salted & hashed password
+	 */
+	async create(username: string, clear_password: string): Promise<Account> {
+		const salt = createSalt();
+		const password = hashPassword(clear_password, salt);
+		const data = await this.sql<Account[]>`
             INSERT INTO accounts (username, password, salt) VALUES (
                 ${username}, ${password}, ${salt}
             ) RETURNING account_id
         `;
-        return data[0];
-    }
+		return data[0];
+	}
 
-    //TODO implement the rest
-    // async read(space_id: number): Promise<Space | undefined> {
-    //     log.debug(`[findById] ${space_id}`);
-    //     const data = await this.sql<Space[]>`
-    //         SELECT space_id, name, icon, view, created, updated
-    //         FROM spaces WHERE space_id=${space_id}
-    //     `;
-    //     log.debug('[findById] result', data);
-    //     return data[0];
-    // }
+	async read(account_id: number): Promise<Account | undefined> {
+		log.debug(`[findById] ${account_id}`);
+		const data = await this.sql<Account[]>`
+			SELECT account_id, username, created, updated
+			FROM accounts WHERE account_id=${account_id}
+		`;
+		log.debug('[findById] result', data);
+		return data[0];
+	}
 
-    // async getAll(): Promise<Space[]> {
-    //     log.debug(`[getAll] Spaces`);
-    //     const data = await this.sql<Space[]>`
-    //         SELECT space_id, name, icon, view, created, updated
-    //         FROM spaces;
-    //     `;
-    //     log.debug('[getAll] Spaces result', data);
-    //     return data;   
-    // }
+	async getAll(): Promise<Account[]> {
+		log.debug(`[getAll] Accounts`);
+		const data = await this.sql<Account[]>`
+            SELECT account_id, username, created, updated
+            FROM accounts;
+        `;
+		log.debug('[getAll] Accounts result', data);
+		return data;
+	}
 
-    // async update(
-    //     space_id: number,
-    //     partial: Partial<Pick<Space, 'name' | 'icon' | 'view'>>,
-    // ): Promise<Space> {
-    //     log.debug(`updating data for space: ${space_id}`);
-    //     const data = await this.sql<Space[]>`
-    //         UPDATE spaces
-    //         SET updated=NOW(), ${this.sql(partial as any, ...Object.keys(partial))}
-    //         WHERE space_id=${space_id}
-    //         RETURNING *
-    //     `;
-    //     if (!data.count) throw Error('no space found');
-    //     return data[0];
-    // }
+	async update(
+		account_id: number,
+		partial: Partial<Pick<Account, 'username' | 'password'>>
+	): Promise<Account> {
+		log.debug(`updating data for account: ${account_id}`);
+		const data = await this.sql<Account[]>`
+			UPDATE accounts
+			SET updated=NOW(), ${this.sql(partial as any, ...Object.keys(partial))}
+			WHERE account_id=${account_id}
+			RETURNING *
+		`;
+		if (!data.count) throw Error('no account found');
+		return data[0];
+	}
 
-    // async delete(space_id: number): Promise<void> {
-    //     log.debug('[deleteById]', space_id);
-    //     const data = await this.sql`
-    //         DELETE FROM spaces WHERE space_id=${space_id}
-    //     `;
-    //     if (!data.count) throw Error('no space found');
-    // }
-
+	async delete(account_id: number): Promise<void> {
+		log.debug('[deleteById]', account_id);
+		const data = await this.sql`
+			DELETE FROM accounts WHERE account_id=${account_id}
+		`;
+		if (!data.count) throw Error('no account found');
+	}
 }
