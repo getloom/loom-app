18daf3e2bc1d3c0a81510e644a9493e7d67b2b6c
greatbacon <hamiltonjreed@gmail.com>
2026-08-20 19:30:30 -0600

    Remove "accounts" and go SSO only

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,12 +1,7 @@
-import { defaultPostgresOptions } from '$lib/db/postgres.server';
-import { AccountRepo } from '$lib/system/accounts/accountsRepo';
-import postgres from 'postgres';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
-	const users = await new AccountRepo(postgres(defaultPostgresOptions)).getAll();
 	return {
-		users: users,
-		isAuthenticated: !!(locals.account_id || locals.keycloakSubject)
+		isAuthenticated: !!locals.keycloakSubject
 	};
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,8 @@
 <script lang="ts">
-	import type { Account } from '$lib/system/accounts/accountsService';
 	import { Button } from 'svelte-ux';
-
-	let {data } = $props()
-
-	//need to figure out how to make this a store/rune/etc/reactive/etc.
-	let users = data?.users ? data.users : []
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <Button variant="outline">Click me!</Button>
-
-	{#if users.length === 0}
-		<div>no users found</div>
-	{/if}
-	{#each users as {username, created}}
-		<div>{username} -- {created}</div>
-	{/each}
